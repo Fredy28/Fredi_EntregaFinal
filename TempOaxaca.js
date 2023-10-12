30577159202c77edd1a8caf38770fe098bba2e38
Fredi <fredinavarrete19@gmail.com>
2023-10-12 11:54:18 -0600

    Repositorio 2

--- TempOaxaca.js
+++ TempOaxaca.js
@@ -1,3 +1,8 @@
 const datos = [];
 const ctx = document.getElementById("grafico").getContext("2d");
 let grafico;
+
+function calcularTemp() {
+    const dia = document.getElementById("dia").value;
+    const temperatura = parseFloat(document.getElementById("temperatura").value)
+};
